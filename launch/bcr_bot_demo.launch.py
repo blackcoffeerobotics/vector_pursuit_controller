3e6a844e40c4198a92df8fb37f09b3e1723a5a57
Arthur <arthurgomes2727@gmail.com>
2024-08-08 07:43:21 +0530

    new tutorial added with launch and config in repo, readme updated

---
+++ launch/bcr_bot_demo.launch.py
@@ -0,0 +1,100 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_vp = get_package_share_directory('vector_pursuit_controller')
+    pkg_bcr_bot = get_package_share_directory('bcr_bot')
+
+    remappings = [('/tf', 'tf'),
+        ('/tf_static', 'tf_static')]
+    
+    params_file = os.path.join(pkg_vp, 'config', 'sample_nav2_params.yaml')
+    
+    bcr_bot_gz_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(pkg_bcr_bot, 'launch', 'gz.launch.py'))
+    )
+
+    controller_server = Node(
+        package='nav2_controller',
+        executable='controller_server',
+        output='screen',
+        parameters=[params_file],
+        remappings=remappings + [('cmd_vel', 'bcr_bot/cmd_vel'),
+                                 ('odom', 'bcr_bot/odom')])
+    
+    planner_server = Node(
+        package='nav2_planner',
+        executable='planner_server',
+        name='planner_server',
+        output='screen',
+        parameters=[params_file],
+        remappings=remappings)
+    
+    behavior_server = Node(
+        package='nav2_behaviors',
+        executable='behavior_server',
+        name='behavior_server',
+        output='screen',
+        parameters=[params_file],
+        remappings=remappings)
+    
+    bt_navigator = Node(
+        package='nav2_bt_navigator',
+        executable='bt_navigator',
+        name='bt_navigator',
+        output='screen',
+        parameters=[params_file],
+        remappings=remappings)
+    
+    lifecycle_manager = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager',
+        output='screen',
+        parameters=[params_file])
+
+    rviz_launch_cmd = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=[
+            '-d' + os.path.join(
+                get_package_share_directory('nav2_bringup'),
+                'rviz',
+                'nav2_default_view.rviz'
+            )
+        ]
+    )
+
+    map_server_node = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        output='screen',
+        parameters=[{'yaml_filename': os.path.join(pkg_vp, 'config', 'bcr_map.yaml')}],
+    )
+
+    static_transform_publisher_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='map_to_odom',
+        output='screen',
+        arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom']
+    )
+
+    return LaunchDescription([
+        bcr_bot_gz_launch,
+        controller_server,
+        planner_server,
+        behavior_server,
+        bt_navigator,
+        lifecycle_manager,
+        rviz_launch_cmd,
+        map_server_node,
+        static_transform_publisher_node
+    ])
+
