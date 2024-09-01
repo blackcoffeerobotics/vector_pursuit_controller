3a5fc5c1caef0b978e3a6fd632367b8a4c223b69
exMachina316 <kostubh@blackcoffeerobotics.com>
2024-09-01 12:05:51 +0530

    Fixed liniting issues

--- launch/bcr_bot_demo.launch.py
+++ launch/bcr_bot_demo.launch.py
@@ -5,15 +5,16 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     pkg_vp = get_package_share_directory('vector_pursuit_controller')
     pkg_bcr_bot = get_package_share_directory('bcr_bot')
 
     remappings = [('/tf', 'tf'),
-        ('/tf_static', 'tf_static')]
-    
+                  ('/tf_static', 'tf_static')]
+
     params_file = os.path.join(pkg_vp, 'config', 'sample_nav2_params.yaml')
-    
+
     bcr_bot_gz_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(pkg_bcr_bot, 'launch', 'gz.launch.py'))
     )
@@ -25,7 +26,7 @@ def generate_launch_description():
         parameters=[params_file],
         remappings=remappings + [('cmd_vel', 'bcr_bot/cmd_vel'),
                                  ('odom', 'bcr_bot/odom')])
-    
+
     planner_server = Node(
         package='nav2_planner',
         executable='planner_server',
@@ -33,7 +34,7 @@ def generate_launch_description():
         output='screen',
         parameters=[params_file],
         remappings=remappings)
-    
+
     behavior_server = Node(
         package='nav2_behaviors',
         executable='behavior_server',
@@ -41,7 +42,7 @@ def generate_launch_description():
         output='screen',
         parameters=[params_file],
         remappings=remappings)
-    
+
     bt_navigator = Node(
         package='nav2_bt_navigator',
         executable='bt_navigator',
@@ -49,7 +50,7 @@ def generate_launch_description():
         output='screen',
         parameters=[params_file],
         remappings=remappings)
-    
+
     lifecycle_manager = Node(
         package='nav2_lifecycle_manager',
         executable='lifecycle_manager',
@@ -97,4 +98,3 @@ def generate_launch_description():
         map_server_node,
         static_transform_publisher_node
     ])
-
